e19a1e672af2ddcaf221c93f741d58a123b02f9f
Wissal <wissal1zabour@gmail.com>
2026-02-19 02:48:45 +0100

    src file

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use(cors());
 
 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use('/api/products', productsRouter);
 app.use('/api/admin/products', productsAdminRouter);
 
